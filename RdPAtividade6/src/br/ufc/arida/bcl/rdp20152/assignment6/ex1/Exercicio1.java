297b9436f742e7a5ae877cd557589a26380babda
brunoleal <ibrunoleal@gmail.com>
2015-12-09 14:57:02 -0300

    mais funcionalidadews do exercicio 1

--- RdPAtividade6/src/br/ufc/arida/bcl/rdp20152/assignment6/ex1/Exercicio1.java
+++ RdPAtividade6/src/br/ufc/arida/bcl/rdp20152/assignment6/ex1/Exercicio1.java
@@ -1,6 +1,5 @@
 package br.ufc.arida.bcl.rdp20152.assignment6.ex1;
 
-import org.apache.commons.math3.linear.MatrixUtils;
 import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.RealVector;
 
@@ -16,9 +15,11 @@ public class Exercicio1 {
 		RealMatrix X = f.getDermatolgy_data().getSubMatrix(0, f.getDermatolgy_data().getRowDimension() -1, 0, f.getDermatolgy_data().getColumnDimension() - 2);
 		RealVector l = f.getDermatolgy_data().getColumnVector(f.getDermatolgy_data().getColumnDimension() -1);
 		
-		RealMatrix T = f.getMatrizT(l, 6);
+		RealMatrix T = f.formarMatrizT(l, 6);
 		
+		RealMatrix X1 = f.matrixToZeroMeanAndVariance(X);
 		
+		f.matrixToRandomMatrixRows(X);
 	}
 
 }
--- RdPAtividade6/src/br/ufc/arida/bcl/rdp20152/assignment6/ex1/Exercicio1Functions.java
+++ RdPAtividade6/src/br/ufc/arida/bcl/rdp20152/assignment6/ex1/Exercicio1Functions.java
@@ -1,9 +1,17 @@
 package br.ufc.arida.bcl.rdp20152.assignment6.ex1;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.ArrayRealVector;
 import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.RealVector;
+import org.apache.commons.math3.stat.descriptive.moment.Mean;
+import org.apache.commons.math3.stat.descriptive.moment.Variance;
+import org.apache.commons.math3.stat.descriptive.moment.VectorialMean;
 
 import br.ufc.arida.bcl.rdp20152.assignment6.arquivos.FileHandler;
 
@@ -22,7 +30,7 @@ public class Exercicio1Functions {
 		return dermatolgy_data;
 	}
 	
-	public RealMatrix getMatrizT(RealVector labels, int numeroDeAtributos) {
+	public RealMatrix formarMatrizT(RealVector labels, int numeroDeAtributos) {
 		RealMatrix T = new Array2DRowRealMatrix(labels.getDimension(), numeroDeAtributos);
 		for (int i = 0; i < labels.getDimension(); i++) {
 			int label = (int)labels.getEntry(i);
@@ -37,6 +45,36 @@ public class Exercicio1Functions {
 			T.setRowVector(i, vtemp);
 		}
 		return T;
+	}
+	
+	public RealMatrix matrixToZeroMeanAndVariance(RealMatrix matrix) {
+		RealMatrix R = new Array2DRowRealMatrix(matrix.getRowDimension(), matrix.getColumnDimension());
+		for (int j = 0; j < matrix.getColumnDimension(); j++) {
+			Mean mean = new Mean();
+			mean.setData(matrix.getColumnVector(j).toArray());
+			double u = mean.evaluate();
+			
+			Variance variance = new Variance();
+			variance.setData(matrix.getColumnVector(j).toArray());
+			double v = variance.evaluate();
+			
+			RealVector colunaj =  matrix.getColumnVector(j).mapDivide(u).mapDivide(v);
+			R.setColumnVector(j, colunaj);
+		}
+		return R;
+	}
+	
+	public void matrixToRandomMatrixRows(RealMatrix matrix) {
+		//RealMatrix R = new Array2DRowRealMatrix(matrix.getRowDimension(), matrix.getColumnDimension());
+		
+		List<Integer> listaDePosicoes = new ArrayList<Integer>();
+		for (int i = 0; i < matrix.getRowDimension(); i++) {
+			listaDePosicoes.add(i);
+		}
+		Collections.shuffle(listaDePosicoes);
+		System.out.println(listaDePosicoes);
+		
+		//return R;
 	}
 
 }
