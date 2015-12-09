04b1d5370726fc95536c531e61f1ff04963562ca
ibrunoleal <ibrunoleal@gmail.com>
2015-12-09 13:31:54 -0300

    -

--- RdPAtividade6/src/br/ufc/arida/bcl/rdp20152/assignment6/ex1/Exercicio1.java
+++ RdPAtividade6/src/br/ufc/arida/bcl/rdp20152/assignment6/ex1/Exercicio1.java
@@ -1,9 +1,11 @@
 package br.ufc.arida.bcl.rdp20152.assignment6.ex1;
 
+import org.apache.commons.math3.linear.MatrixUtils;
 import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.RealVector;
 
 import br.ufc.arida.bcl.rdp20152.assignment6.entidades.Matriz;
+
 
 public class Exercicio1 {
 
@@ -11,9 +13,10 @@ public class Exercicio1 {
 		
 		Exercicio1Functions f = new Exercicio1Functions();
 		
-		RealMatrix M = f.getDermatolgy_data().getSubMatrix(0, f.getDermatolgy_data().getRowDimension() -1, 0, f.getDermatolgy_data().getColumnDimension() - 2);
-		RealVector t = f.getDermatolgy_data().getColumnVector(f.getDermatolgy_data().getColumnDimension() -1);
+		RealMatrix X = f.getDermatolgy_data().getSubMatrix(0, f.getDermatolgy_data().getRowDimension() -1, 0, f.getDermatolgy_data().getColumnDimension() - 2);
+		RealVector l = f.getDermatolgy_data().getColumnVector(f.getDermatolgy_data().getColumnDimension() -1);
 		
+		RealMatrix T = f.getMatrizT(l, 6);
 		
 		
 	}
--- RdPAtividade6/src/br/ufc/arida/bcl/rdp20152/assignment6/ex1/Exercicio1Functions.java
+++ RdPAtividade6/src/br/ufc/arida/bcl/rdp20152/assignment6/ex1/Exercicio1Functions.java
@@ -1,7 +1,9 @@
 package br.ufc.arida.bcl.rdp20152.assignment6.ex1;
 
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.ArrayRealVector;
 import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
 
 import br.ufc.arida.bcl.rdp20152.assignment6.arquivos.FileHandler;
 
@@ -20,6 +22,21 @@ public class Exercicio1Functions {
 		return dermatolgy_data;
 	}
 	
-	
+	public RealMatrix getMatrizT(RealVector labels, int numeroDeAtributos) {
+		RealMatrix T = new Array2DRowRealMatrix(labels.getDimension(), numeroDeAtributos);
+		for (int i = 0; i < labels.getDimension(); i++) {
+			int label = (int)labels.getEntry(i);
+			RealVector vtemp = new ArrayRealVector(numeroDeAtributos);
+			for (int j = 0; j < numeroDeAtributos; j++) {
+				if ( (label - 1) == j) {
+					vtemp.setEntry(j, 1);
+				} else {
+					vtemp.setEntry(j, -1);
+				}
+			}
+			T.setRowVector(i, vtemp);
+		}
+		return T;
+	}
 
 }
