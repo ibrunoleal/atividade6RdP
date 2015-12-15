760746790d69418927097ca335c88af870d3ffe5
ibrunoleal <ibrunoleal@gmail.com>
2015-12-15 15:29:55 -0300

    -

--- RdPAtividade6/src/br/ufc/arida/bcl/rdp20152/assignment6/arquivos/FileHandler.java
+++ RdPAtividade6/src/br/ufc/arida/bcl/rdp20152/assignment6/arquivos/FileHandler.java
@@ -1,48 +1,53 @@
 package br.ufc.arida.bcl.rdp20152.assignment6.arquivos;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.LineNumberReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileHandler {
 	
 	/**
-	 * nome do arquivo que contem o dataset
+	 * nome(ou path) do arquivo que contem o dataset
 	 */
 	private String arquivo;
 	
 	/**
 	 * Separador de elementos da linha
-	 * obs: o separador entre linhas é a quebra de linha por padrao
+	 * obs: o separador entre linhas é a quebra de linha por padrao("\n")
 	 */
 	private String delimitador;
 	
+	/**
+	 * Cria um gerenciador de arquivos.
+	 * @param arquivo
+	 * 		Nome (ou PATH) do arquivo
+	 * @param delimitador
+	 * 		Texto que separa um elemento de linha do elemento seguinte.
+	 * 		Exemplo: a "," em arquivos CSV ou um espaço.
+	 */
 	public FileHandler(String arquivo, String delimitador) {
 		this.arquivo = arquivo;
 		this.delimitador = delimitador;
 	}
 	
 	/**
-	 * Contabiliza o numero de linhas contidas no arquivo. Consequentemente o numero de linhas da matriz.
-	 * @return o numero de linhas no arquivo.
+	 * Computa o numero de linhas contidas no arquivo.
+	 * Obs.: se o arquivo representar os dados de uma matriz, computará o numero de linhas de uma matriz.
+	 * @return
+	 * 		o numero de linhas no arquivo.
 	 */
 	public int getNumeroDeLinhas() {
-		FileReader fileReader;
-		int numeroDeLinhas = 0;
-		
 		try {
-			fileReader = new FileReader(arquivo);
-			BufferedReader br = new BufferedReader(fileReader);
-			
-			String linha = br.readLine();
-			while (linha != null) {
-				numeroDeLinhas++;
-				linha = br.readLine();
-			}
-			br.close();
-
+			File arquivoLeitura = new File(arquivo);
+			LineNumberReader linhaLeitura = new LineNumberReader(new FileReader(arquivoLeitura));
+			linhaLeitura.skip(arquivoLeitura.length());
+			int qtdLinha = linhaLeitura.getLineNumber();
+			linhaLeitura.close();
+			return qtdLinha;
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -50,14 +55,15 @@ public class FileHandler {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-				
-		return numeroDeLinhas;
+		return -1;
 	}
 	
 	/**
-	 * Contabiliza o numero de colunas contidas no arquivo a partir do delimitador especificado. 
-	 * Consequentemente o numero de colunas da matriz.
-	 * @return o numero de colunas do arquivo.
+	 * Contabiliza o numero de elementos na primeira linha do arquivo.
+	 * Obs.: se o arquivo representar os dados de uma matriz, computará o numero 
+	 * de colunas de uma matriz.
+	 * @return
+	 * 		o numero de elementos na primeira linha do arquivo.
 	 */
 	public int getNumeroDeColunas() {
 		FileReader fileReader;
@@ -85,8 +91,15 @@ public class FileHandler {
 		return numeroDeColunas;
 	}
 	
-
-	public List<Double> getVetorLista(int coluna) {
+	/**
+	 * Recupera os elementos de uma dada coluna do arquivo.
+	 * @param coluna
+	 * 		O numero da coluna do aquivo do qual serão recuperados os elementos.
+	 * 		Obs.: a primeira coluna é dada por 0.
+	 * @return
+	 * 		Uma List contendo os elementos da coluna dada.
+	 */
+	public List<Double> getColumnAsList(int coluna) {
 		FileReader fileReader;
 		int numeroDeLinhas = getNumeroDeLinhas();
 		List<Double> vetor = new ArrayList<Double>();
@@ -118,8 +131,16 @@ public class FileHandler {
 		return vetor;
 	}
 	
-	public double[] getVetor(int coluna) {
-		List<Double> lista = getVetorLista(coluna);
+	/**
+	 * Recupera os elementos de uma dada coluna do arquivo.
+	 * @param coluna
+	 * 		O numero da coluna do aquivo do qual serão recuperados os elementos.
+	 * 		Obs.: a primeira coluna é dada por 0.
+	 * @return
+	 * 		Um vetor contendo os elementos da coluna dada.
+	 */
+	public double[] getColumnAsArray(int coluna) {
+		List<Double> lista = getColumnAsList(coluna);
 		double[] vetor = new double[lista.size()];
 		for (int i = 0; i < lista.size(); i++) {
 			vetor[i] = lista.get(i);
@@ -127,6 +148,13 @@ public class FileHandler {
 		return vetor;
 	}
 	
+	/**
+	 * Recupera os elementos de um arquivo na forma de Matriz.
+	 * @return
+	 * 		Uma matriz contendo os todos os elementos do arquivo na forma de matriz.
+	 * 		Obs.: as linhas devem ter a mesma quantidade de elementos uma das outras.
+	 * 		Obs.: as colunas devem ter a mesma quantidade de elementos uma das outras.
+	 */
 	public double[][] getMatriz() {
 		int numeroDeLinhas = getNumeroDeLinhas();
 		int numeroDeColunas = getNumeroDeColunas();
@@ -134,15 +162,13 @@ public class FileHandler {
 		
 		if (numeroDeColunas > 0) {
 			for (int j = 0; j < numeroDeColunas; j++) {
-				double[] coluna = getVetor(j);
+				double[] coluna = getColumnAsArray(j);
 				for (int i = 0; i < coluna.length; i++) {
 					matriz[i][j] = coluna[i];
 				}
 			}
 		}
-		
-		return matriz;
-		
+		return matriz;	
 	}
 	
 }
